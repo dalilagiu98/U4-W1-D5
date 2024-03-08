d21e1ce1ce5ef99a55f972298acf35f615d8d3c1
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-03-08 10:30:03 +0100

    creation multimedia item class

---
+++ U4-W1-D5/src/Main.java
@@ -0,0 +1,7 @@
+import entities.MultimediaItem;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ U4-W1-D5/src/entities/MultimediaItem.java
@@ -0,0 +1,13 @@
+package entities;
+
+abstract class MultimediaItem {
+
+    //ATTRIBUTES LIST:
+    private String title;
+
+    //CONSTRUCTORS:
+
+    public MultimediaItem(String title) {
+        this.title = title;
+    }
+}
