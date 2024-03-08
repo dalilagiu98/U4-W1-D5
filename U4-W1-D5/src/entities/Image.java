3390428fb0daf91d07c315f24a724caa2062e011
dalilagiu98 <148708941+dalilagiu98@users.noreply.github.com>
2024-03-08 12:29:35 +0100

    Creating class image

--- U4-W1-D5/src/entities/Image.java
+++ U4-W1-D5/src/entities/Image.java
@@ -1,4 +1,29 @@
 package entities;
 
-public class Image{
+import interfaces.Brightness;
+
+public class Image extends MultimediaItem implements Brightness {
+
+    //ATTRIBUTES LIST:
+    private int brightness;
+
+    //CONSTRUCTOR:
+    public Image(String title, int brightness) {
+        super(title);
+        this.brightness = brightness;
+    }
+
+
+    //INTERFACES METHODS:
+    @Override
+    public void increaseBrightness() {
+        this.brightness++;
+    }
+
+    @Override
+    public void decreaseBrightness() {
+        if (this.brightness > 0) {
+            this.brightness--;
+        }
+    }
 }
